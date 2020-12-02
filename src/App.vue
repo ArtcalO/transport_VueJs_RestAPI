585748bcb981ed5cef08be2a25b0447affb73dba
ArtcalO <niyca14@gmail.com>
2020-12-02 19:55:40 +0100

    Setting pages and components in app.vue and main.js

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
-
+  <div class="container-fluid" >
+    <app-header></app-header>
+    <hr>
+    <div>
+      <h3>Body</h3>
+    </div>
+    <hr>
+    <app-footer></app-footer>
+  </div>
 </template>
 
 <script>
-
+import Header  from './components/shared/header.vue';
+import Footer  from './components/shared/footer.vue';
 
 export default {
-
+  components : {
+    'app-header' : Header,
+    'app-footer' : Footer
+  }
   
 };
 </script>
-
+	
 <style>
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import { routes } from './routes.js';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+	routes,
+	mode : 'history'
+});
 
 
 new Vue({
