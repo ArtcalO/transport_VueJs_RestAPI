71cbeea752f1130b45d9dfcb8a507b26329d9a74
ArtcalO <niyca14@gmail.com>
2020-12-03 19:43:06 +0100

    Adding components

---
+++ src/components/AddTrip.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container" >
+    <h3>AddTrip</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  };
+</script>
+
+<style scoped >
+
+  a{
+    text-decoration: none;
+  }
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import Home from './pages/Home.vue';
 import Register from './pages/Register.vue';
 import Connexion from './pages/Connexion.vue';
 import Admin from './pages/Admin.vue';
+import AddTrip from './components/AddTrip.vue';
 
 export const routes = [
 	{path: '', component: Home},
 	{path: '/register', component: Register},
 	{path: '/connexion', component: Connexion},
+	{path: '/admin/add_trip', component: AddTrip},
 	{path: '/admin', component: Admin}
 ];
