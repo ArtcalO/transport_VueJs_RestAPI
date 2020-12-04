66c7b471b3859389ec0aa4182b765c6d2e430e5e
ArtcalO <niyca14@gmail.com>
2020-12-04 22:34:16 +0100

    Dynamic routes added in routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,15 @@ import Register from './pages/Register.vue';
 import Connexion from './pages/Connexion.vue';
 import Admin from './pages/Admin.vue';
 import AddTrip from './components/AddTrip.vue';
+import AddDriver from './components/AddDriver.vue';
+import AddBus from './components/AddBus.vue';
 
 export const routes = [
 	{path: '', component: Home},
 	{path: '/register', component: Register},
 	{path: '/connexion', component: Connexion},
 	{path: '/admin/add_trip', component: AddTrip},
-	{path: '/admin', component: Admin}
+	{path: '/admin', component: Admin},
+	{path: '/admin/add_driver', component: AddDriver},
+	{path: '/admin/add_bus', component: AddBus}
 ];
