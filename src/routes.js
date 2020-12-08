2dfb57ba1ee59504802120ba97d5273876bcd701
ArtcalO <niyca14@gmail.com>
2020-12-08 07:31:15 +0100

    Routes Js

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Admin from './pages/Admin.vue';
 import AddTrip from './components/AddTrip.vue';
 import AddDriver from './components/AddDriver.vue';
 import AddBus from './components/AddBus.vue';
+import AddPoint from './components/AddPoint.vue';
 
 export const routes = [
 	{path: '', component: Home},
@@ -13,5 +14,6 @@ export const routes = [
 	{path: '/admin/add_trip', component: AddTrip},
 	{path: '/admin', component: Admin},
 	{path: '/admin/add_driver', component: AddDriver},
-	{path: '/admin/add_bus', component: AddBus}
+	{path: '/admin/add_bus', component: AddBus},
+	{path: '/admin/add_scale_point', component: AddPoint},
 ];
