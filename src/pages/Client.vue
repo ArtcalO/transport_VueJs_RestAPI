e606fad446217b13a5004d4265a458c91abe16cc
ArtcalO <niyca14@gmail.com>
2020-12-12 14:11:33 +0100

    Adding client page and bg images

---
+++ src/pages/Client.vue
@@ -0,0 +1,104 @@
+<template>
+	  <div class="container">
+        <div class="formular mb-3">
+			 <form class="form-inline" action="">
+			  <label for="email" class="mr-sm-2">Depart : </label>
+			  <input type="text" class="form-control mb-2 mr-sm-2" placeholder="" >
+			  <label for="text" class="mr-sm-2">Arrivé : </label>
+			  <input type="text" class="form-control mb-2 mr-sm-2" placeholder="" >
+			  
+			  <button type="submit" class="btn btn-primary mb-2">Rechercher</button>
+			</form> 
+        </div>
+
+        <div class="resultSearch">
+        	<div class="card-columns">
+			  <div class="card bg-light">
+			    <div class="card-body text-center">
+			      <p class="card-text">Voyage : Buja Muramva</p>
+			      <p class="card-text">Escales : 
+			      	<span class="badge badge-warning">Bubanza</span>
+			      	<span class="badge badge-primary">Cankuzo</span>
+			      	</p>
+			      	<form method="POST">
+			      		<button class="btn btn-outline-primary">Book Now</button>
+			      	</form>
+			    </div>
+			  </div>
+			  <div class="card bg-light">
+			    <div class="card-body text-center">
+			      <p class="card-text">Voyage : Buja Muramva</p>
+			      <p class="card-text">Escales :
+			      	<span class="badge badge-warning">Bubanza</span>
+			      	<span class="badge badge-primary">Cankuzo</span>
+			      	</p>
+			      	<form method="POST">
+			      		<button class="btn btn-outline-primary">Book Now</button>
+			      	</form>
+			    </div>
+			  </div>
+			  <div class="card bg-light">
+			    <div class="card-body text-center">
+			      <p class="card-text">Voyage : Buja Muramva</p>
+			      <p class="card-text">Escales :
+			      	<span class="badge badge-warning">Bubanza</span>
+			      	<span class="badge badge-primary">Cankuzo</span>
+			      	</p>
+			      	<form method="POST">
+			      		<button class="btn btn-outline-primary">Book Now</button>
+			      	</form>
+			    </div>
+			  </div>
+			  <div class="card bg-light">
+			    <div class="card-body text-center">
+			      <p class="card-text">Voyage : Buja Muramva</p>
+			      <p class="card-text">Escales :
+			      	<span class="badge badge-warning">Bubanza</span>
+			      	<span class="badge badge-primary">Cankuzo</span>
+			      <form method="POST">
+			      		<button class="btn btn-outline-primary">Book Now</button>
+			      	</form>
+			    </div>
+			  </div>
+			  <div class="card bg-light">
+			    <div class="card-body text-center">
+			      <p class="card-text">Voyage : Buja Muramva</p>
+			      <p class="card-text">Escales : 
+			      	<span class="badge badge-warning">Bubanza</span>
+			      	<span class="badge badge-primary">Cankuzo</span>
+			      </p>
+			      <form method="POST">
+			      		<button class="btn btn-outline-primary">Book Now</button>
+			      	</form>
+			    </div>
+			  </div>
+			  <div class="card bg-light">
+			    <div class="card-body text-center">
+			      <p class="card-text">Voyage :Buja-Muramvya</p>
+			      <p class="card-text">Escales : 
+			      	<span class="badge badge-primary">Primary</span>
+			      </p>
+			      <form method="POST">
+			      		<button class="btn btn-outline-primary">Book Now</button>
+			      	</form>
+			    </div>
+			  </div>
+			</div>
+        </div>
+    </div>
+</body>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+	.formular{
+		height: 300px;
+		background-color: cadetblue;
+		padding: 100px 200px;
+	}
+</style>
