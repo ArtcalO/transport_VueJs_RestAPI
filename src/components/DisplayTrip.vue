9fa4d28902abcc9fbcdb7ba848ea62665cb2aff6
ArtcalO <niyca14@gmail.com>
2020-12-13 23:46:55 +0100

    Some components added

---
+++ src/components/DisplayTrip.vue
@@ -0,0 +1,100 @@
+<template>
+	<div>
+		<div class="title mb-5">
+	  		<h4>Les Voyages Reguliers</h4>
+	  	</div>
+
+	  	<div class="container">
+	  	
+        	<div class="card-columns">
+			  <div class="card bg-light">
+			    <div class="card-body text-center">
+			      <p class="card-text">Voyage : Buja Muramva</p>
+			      <p class="card-text">Escales : 
+			      	<span class="badge badge-warning">Bubanza</span>
+			      	<span class="badge badge-primary">Cankuzo</span>
+			      	</p>
+			      	<form method="POST">
+			      		<button class="btn btn-outline-primary">Details</button>
+			      	</form>
+			    </div>
+			  </div>
+			  <div class="card bg-light">
+			    <div class="card-body text-center">
+			      <p class="card-text">Voyage : Buja Muramva</p>
+			      <p class="card-text">Escales :
+			      	<span class="badge badge-warning">Bubanza</span>
+			      	<span class="badge badge-primary">Cankuzo</span>
+			      	</p>
+			      	<form method="POST">
+			      		<button class="btn btn-outline-primary">Details</button>
+			      	</form>
+			    </div>
+			  </div>
+			  <div class="card bg-light">
+			    <div class="card-body text-center">
+			      <p class="card-text">Voyage : Buja Muramva</p>
+			      <p class="card-text">Escales :
+			      	<span class="badge badge-warning">Bubanza</span>
+			      	<span class="badge badge-primary">Cankuzo</span>
+			      	</p>
+			      	<form method="POST">
+			      		<button class="btn btn-outline-primary">Details</button>
+			      	</form>
+			    </div>
+			  </div>
+			  <div class="card bg-light">
+			    <div class="card-body text-center">
+			      <p class="card-text">Voyage : Buja Muramva</p>
+			      <p class="card-text">Escales :
+			      	<span class="badge badge-warning">Bubanza</span>
+			      	<span class="badge badge-primary">Cankuzo</span>
+			      <form method="POST">
+			      		<button class="btn btn-outline-primary">Details</button>
+			      	</form>
+			    </div>
+			  </div>
+			  <div class="card bg-light">
+			    <div class="card-body text-center">
+			      <p class="card-text">Voyage : Buja Muramva</p>
+			      <p class="card-text">Escales : 
+			      	<span class="badge badge-warning">Bubanza</span>
+			      	<span class="badge badge-primary">Cankuzo</span>
+			      </p>
+			      <form method="POST">
+			      		<button class="btn btn-outline-primary">Details</button>
+			      	</form>
+			    </div>
+			  </div>
+			  <div class="card bg-light">
+			    <div class="card-body text-center">
+			      <p class="card-text">Voyage :Buja-Muramvya</p>
+			      <p class="card-text">Escales : 
+			      	<span class="badge badge-primary">Primary</span>
+			      </p>
+			      <form method="POST">
+			      		<button class="btn btn-outline-primary">Details</button>
+			      	</form>
+			    </div>
+			  </div>
+			</div>
+
+
+    </div>
+	</div>
+	  
+</body>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+	.title{
+		width: 200px;
+		border-bottom-style: dotted;
+	}
+</style>
